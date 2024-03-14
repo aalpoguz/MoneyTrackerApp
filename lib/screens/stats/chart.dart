25ee1e86a84f226f537db3bb217249ce27b3c453
Alp Oguz <alpoguz@Alp-MacBook-Pro.local>
2024-03-15 02:55:32 +0300

    stat screen added

--- lib/screens/stats/chart.dart
+++ lib/screens/stats/chart.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -16,25 +17,66 @@ class _MyChartState extends State<MyChart> {
     );
   }
 
+  BarChartGroupData makeGroupData(int x, double y) {
+    return BarChartGroupData(x: x, barRods: [
+      BarChartRodData(
+          toY: y,
+          gradient: LinearGradient(
+            colors: [
+              Theme.of(context).colorScheme.primary,
+              Theme.of(context).colorScheme.secondary,
+              Theme.of(context).colorScheme.tertiary,
+            ],
+            transform: const GradientRotation(pi / 40),
+          ),
+          width: 15,
+          backDrawRodData: BackgroundBarChartRodData(
+              show: true, toY: 5, color: Colors.grey.shade300))
+    ]);
+  }
+
+  List<BarChartGroupData> showingGroups() => List.generate(8, (i) {
+        switch (i) {
+          case 0:
+            return makeGroupData(0, 2);
+          case 1:
+            return makeGroupData(1, 3);
+          case 2:
+            return makeGroupData(2, 2);
+          case 3:
+            return makeGroupData(3, 4.5);
+          case 4:
+            return makeGroupData(4, 3.8);
+          case 5:
+            return makeGroupData(5, 1.5);
+          case 6:
+            return makeGroupData(6, 4);
+          case 7:
+            return makeGroupData(7, 3.8);
+          default:
+            return throw Error();
+        }
+      });
+
   BarChartData mainBarData() {
     return BarChartData(
       titlesData: FlTitlesData(
         show: true,
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: false),
-        ),
+        rightTitles:
+            const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
         bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 38,
+          getTitlesWidget: getTiles,
+        )),
+        leftTitles: AxisTitles(
           sideTitles: SideTitles(
             showTitles: true,
             reservedSize: 38,
-            getTitlesWidget: getTiles,
+            getTitlesWidget: leftTitles,
           ),
-        ),
-        leftTitles: const AxisTitles(
-          sideTitles: SideTitles(showTitles: true),
         ),
       ),
       borderData: FlBorderData(show: false),
@@ -45,18 +87,35 @@ class _MyChartState extends State<MyChart> {
 
   Widget getTiles(double value, TitleMeta meta) {
     const style = TextStyle(
-        color: Colors.grey, fontWeight: FontWeight.bold, fontSize: 14);
+      color: Colors.grey,
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
     Widget text;
 
     switch (value.toInt()) {
       case 0:
         text = const Text('01', style: style);
+        break;
+      case 1:
         text = const Text('02', style: style);
+        break;
+      case 2:
         text = const Text('03', style: style);
+        break;
+      case 3:
         text = const Text('04', style: style);
+        break;
+      case 4:
         text = const Text('05', style: style);
+        break;
+      case 5:
         text = const Text('06', style: style);
+        break;
+      case 6:
         text = const Text('07', style: style);
+        break;
+      case 7:
         text = const Text('08', style: style);
         break;
       default:
@@ -65,7 +124,7 @@ class _MyChartState extends State<MyChart> {
     }
     return SideTitleWidget(
       axisSide: meta.axisSide,
-      space: 14,
+      space: 16,
       child: text,
     );
   }
